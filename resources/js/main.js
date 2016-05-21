9ab05b600fcdc78be6cc3841fb41cee6fef097d3
Karandeep Singh <kdsbatra@gmail.com>
2016-05-20 22:13:18 -0400

    what to commit now

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,16 +1,29 @@
 $(document)
   .ready(function() {
+    var dropdowns = $("#main_content_tabs li a");
     var tabs = $("#main_content_tabs .tab a");
-    tabs.on('click', function() {
-      var tab_name = $(this).attr("href");
+
+    function loadTabs() {
+      var tab_id = $(this).attr("href");
       var main = $('main');
-      if ($(tab_name).length == 0) {
-        loadHtml(main, "resources/html/tutorials/" + tab_name.substring(1,tab_name.length)+".html");
-      }
-    });
+      var tab_name = tab_id.substring(1, tab_id.length)
+      if ($(tab_id).length == 0) {
+        loadHtml(main, "resources/html/tutorials/" + tab_name.substring(1, tab_name.length) + ".html");
+
+        $(this).tabs('select_tab', tab_name);
+        $(this).collapsible({
+          accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        });
+      });
+
+
+    }
+
+    tabs.on('click', loadAndSetTabs);
+    dropdowns.on('click', loadAndSetTabs);
 
     function loadHtml($target, path) {
-      $.get(path, function(response){
+      $.get(path, function(response) {
         $target.append(response);
       });
       //target.append('<object type="text/html" data="'+path+'" ></object>')
