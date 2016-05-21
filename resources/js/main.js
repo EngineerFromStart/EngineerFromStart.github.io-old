889e120ce87e3a65689bd4f4b99317277425e5f2
Karandeep Singh <kdsbatra@gmail.com>
2016-05-20 22:47:49 -0400

    what to commit now what duh

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,10 @@
 $(document)
   .ready(function() {
 
+    loadHtml($('body'), "/resources/sidebar.html", false, {
+      prepend: true
+    })
+
     var dropdowns = $("#main_content_dropdowns li a");
     var tabs_holder = $("#main_content_tabs");
     var tabs = $("#main_content_tabs .tab a");
@@ -10,9 +14,9 @@ $(document)
       var main = $('main');
       var tab_name = tab_id.substring(1, tab_id.length)
       if ($(tab_id).length == 0) {
-        loadHtml(main, "resources/html/tutorials/" + tab_name + ".html", function(){
+        loadHtml(main, "resources/html/tutorials/" + tab_name + ".html", function() {
           tabs_holder.tabs('select_tab', tab_name);
-          $("#"+tab_name + " .collapsible").collapsible({
+          $("#" + tab_name + " .collapsible").collapsible({
             accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
           });
         });
@@ -22,11 +26,16 @@ $(document)
     tabs.on('click', loadAndSetTabs);
     dropdowns.on('click', loadAndSetTabs);
 
-    function loadHtml($target, path, callback) {
+    function loadHtml($target, path, callback, extras) {
       $.get(path, function(response) {
-        $target.append(response);
-        callback.apply(this);
+        if (prepend) {
+
+        } else {
+          $target.append(response);
+        }
+        if (callback){
+          callback.apply($target);
+        }
       });
-      //target.append('<object type="text/html" data="'+path+'" ></object>')
     };
   });
