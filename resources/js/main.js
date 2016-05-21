f01d7ca5096c73956a2812595d8d6194baca8448
Karandeep Singh <kdsbatra@gmail.com>
2016-05-20 20:51:58 -0400

    load scripts

---
+++ resources/js/main.js
@@ -0,0 +1,16 @@
+$(document)
+  .ready(function() {
+    var tabs = $("#main_content_tabs .tab a");
+    tabs.on('click', function() {
+      var tab_name = $(this).attr("href");
+      var main = $('main');
+      if ($(tab_name).length == 0) {
+        loadHtml(main, "resources/html/tutorials/" + tab_name.substring(1,tab_name.length)+".html");
+      }
+    });
+
+    function loadHtml($target, path) {
+      $target.load(path);
+      //target.append('<object type="text/html" data="'+path+'" ></object>')
+    };
+  });
